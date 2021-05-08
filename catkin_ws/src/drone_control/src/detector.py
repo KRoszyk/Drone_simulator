dd3cb2d5f3265c1eec8e43a71ecc522a2692c614
KRoszyk <kamil.roszyk22@gmail.com>
2021-05-08 20:58:27 +0200

    adding package to detect Aruco markers in OpenCV

---
+++ catkin_ws/src/drone_control/src/detector.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+import cv2.aruco as aruco
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class Detector:
+    def __init__(self):
+        self.sub = rospy.Subscriber("/ardrone/bottom/image_raw", Image, self.callback)
+        self.pub = rospy.Publisher('/drone_control/image_raw', Image, queue_size=1)
+        self.bridge = CvBridge()
+        self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        self.arucoParams = aruco.DetectorParameters_create()
+
+    def callback(self, image_message):
+        cv_image = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='passthrough')
+        (corners, ids, rejected) = aruco.detectMarkers(cv_image, self.aruco_dict, parameters=self.arucoParams)
+        try:
+            if len(ids) > 0:
+                aruco.drawDetectedMarkers(cv_image, corners, ids)
+        except: pass
+        image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
+        self.pub.publish(image_message)
+
+
+if __name__ == '__main__':
+    rospy.init_node('detection_node')
+    t = Detector()
+    while not rospy.is_shutdown():
+        rospy.spin()
