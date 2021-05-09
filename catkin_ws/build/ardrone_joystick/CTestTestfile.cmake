5c5ff0dc45bef4084ab8bfb26a81d54e2ad4149a
KRoszyk <kamil.roszyk22@gmail.com>
2021-05-09 22:48:47 +0200

    removing files

--- catkin_ws/build/ardrone_joystick/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/share/catkin_ws/src/ardrone_joystick
-# Build directory: /root/share/catkin_ws/build/ardrone_joystick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
